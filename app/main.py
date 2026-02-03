d39f7c155050cde071b8b9cb8b8280bd67e8c060
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-03 14:30:55 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI, Request
 from app.routes import router
-from app.logging import logger
+from app.middleware.logging import logging_middleware
 
 app = FastAPI()
 
 app.include_router(router)
 
+app.middleware("http")(logging_middleware)
 
 @app.get("/")
-def base_response(request : Request):
-    logger.info("Request recieved", extra={
-        "method" : request.method, 
-        "path" : request.url.path, 
-        "client" : request.client.host if request.client else None
-        })
+def base_response():
     return {"message":"Hey! This is EGO Services API. Visit /docs to learn about our API!"}
