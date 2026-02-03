f38b9eeb209c5b5ca53fda01150d3f2574d81125
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-03 13:08:53 -0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# ENV VAR
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+
+# Services
+
+SERVICES = [
+    {"name": "www.ego-services.com", "type": "http", "url": "https://www.ego-services.com"},
+    {"name": "backdoor.ego-services.com", "type": "http", "url": "https://backdoor.ego-services.com/health"},
+    {"name": "fingcraft.ego-services.com", "type": "tcp", "host": "fingcraft.ego-services.com", "port": 25565},
+    {"name": "ssh.ego-services.com", "type": "tcp", "host": "ssh.ego-services.com", "port": 22},
+]
