f162cd9e9d38a3a21e929b113c80d79c09f7135f
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-03 14:30:44 -0300

    Better Logging

--- app/logging.py
+++ app/logging.py
@@ -2,9 +2,12 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(levelname)s] - %(asctime)s - %(message)s | "
-           "Method = %(method)s URL = %(path)s Client = %(client)s"
+    format=(
+        "[%(levelname)s] - %(asctime)s - %(message)s | "
+        "Method = %(method)s | "
+        "Path = %(path)s | "
+        "Client = %(client)s | "
+    ),
 )
 
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger("egos-api")
