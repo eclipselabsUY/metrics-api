9fd110df78cc80701fa9d971d6161b410baefa79
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-03 14:30:30 -0300

    Added APIKey and RCON PSWD

--- app/config.py
+++ app/config.py
@@ -13,3 +13,7 @@ SERVICES = [
     {"name": "fingcraft.ego-services.com", "type": "tcp", "host": "fingcraft.ego-services.com", "port": 25565},
     {"name": "ssh.ego-services.com", "type": "tcp", "host": "ssh.ego-services.com", "port": 22},
 ]
+
+API_KEY = os.getenv("API_KEY")
+
+RCON_PASSWORD = os.getenv("RCON_PASSWORD")
