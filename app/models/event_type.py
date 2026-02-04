d1349c924a2425397bc5f0fa5c7944b4427efb60
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-04 01:34:03 -0300

    Event Type Model

---
+++ app/models/event_type.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class EventType(Base):
+    __tablename__ = "event_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    description = Column(String(255), nullable=True)
+
+    # Relación con Event
+    events = relationship("Event", back_populates="event_type")
