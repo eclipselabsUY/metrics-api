a5a73067a4db80438f2cf53ade0c841d53026e76
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-05 16:49:51 -0300

    Update event.py

--- app/models/event.py
+++ app/models/event.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from sqlalchemy import JSON  # solo para Postgres
+from sqlalchemy import JSON 
 from app.core.database import Base
 
 
