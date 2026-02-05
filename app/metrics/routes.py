c18220744ffa851ad141d22807b240d39c377bb9
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-05 16:49:20 -0300

    Update routes.py

---
+++ app/metrics/routes.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Request, Depends
+
+from app.metrics.schema import EventIn
+from app.metrics.services import create_event
+from app.services.auth import validate_api_key
+
+router = APIRouter()
+
+@router.post("/metrics/event", dependencies=Depends(validate_api_key))
+def new_event(request : Request, event : EventIn):
+    event_data = {
+        "method" : request.method,
+        "url" : request.url.path,
+        "cookies" : request.cookies,
+        "client_ip" : request.client.host,
+        "event_type" : event.event_type,
+        "metadata" : event.metadata 
+    }
+
+    create_event(event_data)
