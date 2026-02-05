1fa9c2785f8cda24dc44b51c7ff9ed508f5c33a2
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-02-05 16:49:04 -0300

    Created proper async and sync engines

--- app/core/database.py
+++ app/core/database.py
@@ -1,19 +1,42 @@
-from app.core.config import DATABASE_URL, JETBASE_SQLALCHEMY_URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from contextlib import contextmanager
 
+from app.core.config import DATABASE_URL
 
 class Base(DeclarativeBase):
     pass
 
+# Async DB
 
-sync_url = JETBASE_SQLALCHEMY_URL if JETBASE_SQLALCHEMY_URL else DATABASE_URL
+async_engine = create_async_engine(DATABASE_URL, echo = False, future = True)
 
-sync_engine = create_engine(sync_url)
-SyncSessionLocal = sessionmaker(bind=sync_engine, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(
+    bind = async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
+async def get_async_db():
+    async with AsyncSessionLocal() as session:
+        yield session
 
-def start_db_engine():
-    engine = create_async_engine(DATABASE_URL)
-    return engine
+# Sync DB
+
+SYNC_DATABASE_URL = DATABASE_URL.replace("+aiosqlite", "").replace("+asyncpg", "")
+
+sync_engine = create_engine(SYNC_DATABASE_URL, echo=False, future=True)
+
+SyncSessionLocal = sessionmaker(
+    bind = sync_engine,
+    expire_on_commit=False
+)
+
+@contextmanager
+def get_sync_db():
+    db = SyncSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
