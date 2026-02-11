c650b74b64e774591694984b4bb839c68bf21ed1
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-11 00:58:23 -0300

    Update service.py

--- app/models/service.py
+++ app/models/service.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from app.core.database import Base
+from app.core.enums import ServiceKind
 
 class ServiceType(Base):
     __tablename__ = "service_types"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), unique=True, nullable=False)
+    kind = Column(Enum(ServiceKind, name="service_kind_enum"),
+        nullable=False,
+        index=True,
+    )    
     services = relationship(
         "Service",
         back_populates="service_type",
@@ -20,9 +25,15 @@ class Service(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50))
     url = Column(String(150))
+    api_keys = relationship(
+        "ApiKey",
+        back_populates="service",
+        cascade="all, delete-orphan",
+        lazy="selectin"
+    )
     service_type_id = Column(
         Integer, 
-        ForeignKey("services_types.id", ondelete="RESTRICT"), 
+        ForeignKey("service_types.id", ondelete="RESTRICT"), 
         nullable=False
     )
     service_type = relationship(
@@ -33,3 +44,6 @@ class Service(Base):
 
     def get_type(self) -> str:
         return self.service_type.name
+    
+    def get_kind(self) -> str:
+        return self.service_type.kind.value
