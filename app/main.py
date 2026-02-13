0d4dae4f5452cd6606f98fbe03dcade1811ed3d6
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-13 02:11:51 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Request
+import app.models
+
+from fastapi import FastAPI
 from app.status import status_router
 from app.middleware.logging import logging_middleware
+from app.core.database import async_engine, Base
+from app.crud import crud_router
+
+async def lifespan(app: FastAPI):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
 app = FastAPI()
 
 app.include_router(status_router)
+
+app.include_router(crud_router)
 
 app.middleware("http")(logging_middleware)
 
