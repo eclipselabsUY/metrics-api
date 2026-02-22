fb268e324a19cdb7d5ce9b8bfc9d2f89a5c1cf50
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-22 13:06:00 -0300

    Update event_types.py

---
+++ app/crud/event_types.py
@@ -0,0 +1,18 @@
+from fastcrud import crud_router, FastCRUD
+
+from app.core.database import get_async_db
+from app.schemas.event_type import EventTypeCreate, EventTypeRead, EventTypeUpdate
+from app.models.event_type import EventType
+
+event_type_crud = FastCRUD(EventType)
+
+event_type_router = crud_router(
+    session=get_async_db,
+    model=EventType,
+    create_schema=EventTypeCreate,
+    update_schema=EventTypeUpdate,
+    select_schema=EventTypeRead,
+    crud=event_type_crud,
+    path="",
+    tags=["EventType"]
+)
