75d9a5b81c0ad8f86667482bea7f89c2790fa91f
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-02-22 13:06:27 -0300

    Update 0002_auto_generated.sql

--- migrations/0002_auto_generated.sql
+++ migrations/0002_auto_generated.sql
@@ -1,13 +1,7 @@
 -- upgrade
 
-CREATE TABLE IF NOT EXISTS api_keys (
-    id INTEGER NOT NULL PRIMARY KEY,
-    key_hash VARCHAR(255) NOT NULL UNIQUE,
-    prefix VARCHAR(32) NOT NULL,
-    service_id INTEGER NOT NULL REFERENCES services(id),
-    is_active BOOLEAN NOT NULL DEFAULT TRUE
-)
+ALTER TABLE api_keys ADD COLUMN prefix VARCHAR(32) NOT NULL
 
 -- rollback
 
-DROP TABLE api_keys
+ALTER TABLE api_keys DROP COLUMN prefix
