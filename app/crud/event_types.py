bf6a5a7d2183e387005765f22f4d8a59b25535d7
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-04-06 20:41:41 -0300

    EventType CRUD

--- app/crud/event_types.py
+++ app/crud/event_types.py
@@ -3,6 +3,7 @@ from fastcrud import crud_router, FastCRUD
 from app.core.database import get_async_db
 from app.schemas.event_type import EventTypeCreate, EventTypeRead, EventTypeUpdate
 from app.models.event_type import EventType
+from app.core.security import verify_admin_key
 
 event_type_crud = FastCRUD(EventType)
 
@@ -14,6 +15,17 @@ event_type_router = crud_router(
     select_schema=EventTypeRead,
     crud=event_type_crud,
     path="",
-    endpoint_names={"read" : "get", "read_multi" : "get", "update" : "update", "delete" : "delete", "create" : "create"},
-    tags=["EventType"]
+    endpoint_names={
+        "read": "get",
+        "read_multi": "get",
+        "update": "update",
+        "delete": "delete",
+        "create": "create",
+    },
+    tags=["EventType"],
+    create_deps=[verify_admin_key],
+    update_deps=[verify_admin_key],
+    delete_deps=[verify_admin_key],
+    read_deps=[verify_admin_key],
+    read_multi_deps=[verify_admin_key],
 )
