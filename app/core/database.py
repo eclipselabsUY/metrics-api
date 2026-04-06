d35cde93681459b2ed164c1058b4c302ca77e790
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-04-06 20:41:17 -0300

    ClickHouse Connection

--- app/core/database.py
+++ app/core/database.py
@@ -2,25 +2,35 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from contextlib import contextmanager
+from aiochclient import ChClient
+from aiohttp import ClientSession
 
-from app.core.config import DATABASE_URL
+from app.core.config import (
+    DATABASE_URL,
+    CLICKHOUSE_URL,
+    CLICKHOUSE_DB,
+    CLICKHOUSE_USER,
+    CLICKHOUSE_PASSWORD,
+)
+
 
 class Base(DeclarativeBase):
     pass
 
+
 # Async DB
 
-async_engine = create_async_engine(DATABASE_URL, echo = False, future = True)
+async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
 AsyncSessionLocal = async_sessionmaker(
-    bind = async_engine,
-    class_=AsyncSession,
-    expire_on_commit=False
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False
 )
+
 
 async def get_async_db():
     async with AsyncSessionLocal() as session:
         yield session
+
 
 # Sync DB
 
@@ -28,10 +38,8 @@ SYNC_DATABASE_URL = DATABASE_URL.replace("+aiosqlite", "").replace("+asyncpg", "
 
 sync_engine = create_engine(SYNC_DATABASE_URL, echo=False, future=True)
 
-SyncSessionLocal = sessionmaker(
-    bind = sync_engine,
-    expire_on_commit=False
-)
+SyncSessionLocal = sessionmaker(bind=sync_engine, expire_on_commit=False)
+
 
 @contextmanager
 def get_sync_db():
@@ -40,3 +48,31 @@ def get_sync_db():
         yield db
     finally:
         db.close()
+
+
+# ClickHouse DB
+
+_ch_session = None
+_ch_client = None
+
+
+async def get_clickhouse_client() -> ChClient:
+    global _ch_client, _ch_session
+    if _ch_client is None:
+        _ch_session = ClientSession()
+        _ch_client = ChClient(
+            _ch_session,
+            url=CLICKHOUSE_URL,
+            user=CLICKHOUSE_USER,
+            password=CLICKHOUSE_PASSWORD,
+            database=CLICKHOUSE_DB,
+        )
+    return _ch_client
+
+
+async def close_clickhouse_client():
+    global _ch_client, _ch_session
+    if _ch_session:
+        await _ch_session.close()
+        _ch_session = None
+        _ch_client = None
