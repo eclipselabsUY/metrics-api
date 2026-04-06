40f008b8b7bd3a47e8a33fa4125c08b48defc2d3
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-04-06 20:52:38 -0300

    Update auth.py

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,19 +1,20 @@
 from fastapi import Request, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.services import find_service_by_api_key
+from app.crud.services import find_service_by_apikey
 
 from app.core.database import get_async_db
 
-def validate_api_key(request: Request, db : AsyncSession = Depends(get_async_db)):
+
+def validate_api_key(request: Request, db: AsyncSession = Depends(get_async_db)):
     api_key = request.headers.get("X-API-Key")
 
     if not api_key:
         raise HTTPException(401)
-    
+
     service = find_service_by_api_key(db, api_key)
 
     if not service:
         raise HTTPException(403)
-    
+
     request.state.service = service
